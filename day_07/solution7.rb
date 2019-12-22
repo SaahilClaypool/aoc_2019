c2b4b40c15b0f45c1863381a030ebb97ea552cd1
Saahil Claypool <sclaypool@hyannisportresearch.com>
2019-12-22 13:35:05 -0500

    day 7 part b

--- day_07/solution7.rb
+++ day_07/solution7.rb
@@ -11,7 +11,7 @@ def parse_input(input_file = './input.txt')
 end
 
 class Program
-  attr_accessor :pc, :mem
+  attr_accessor :pc, :mem, :last_output
 
   def initialize(mem, inp = 1, input_list = nil, output_list = nil, id = 'p')
     @mem = mem
@@ -68,7 +68,7 @@ class Program
     when 4 # output
       a, ai, am = param(modes[0])
       self.output(a)
-      return a.zero?
+      # return a.zero? # TODO this might break old programs
     when 5 # jump if true
       a, ai, am = param(modes[0])
       b, bi, bm = param(modes[1])
@@ -100,7 +100,7 @@ class Program
         @mem[c] = 0
       end
     else
-      p "halt with #{opcode}, #{@id}"
+      # p "halt with #{opcode}, #{@id}"
       output(@last_output)
       return false
     end
@@ -113,11 +113,11 @@ class Program
   def input
     if @input_list && @first_inp
       val = @input_list.next
-      puts "#{@id} next #{val}"
+      # puts "#{@id} next #{val}"
       val
     else
       @first_inp = true
-      puts "#{@id} got first #{@inp}"
+      # puts "#{@id} got first #{@inp}"
       @inp
     end
   end
@@ -128,7 +128,7 @@ class Program
     if @output_list
       @output_list.push(val)
     else 
-      puts "output: #{val}"
+      # puts "output: #{val}"
     end
   end
 
@@ -140,6 +140,7 @@ class Program
   def run_async
     handle = Thread.new {
       while step; end
+      # puts "thread #{@id} over"
     }
     handle
   end
@@ -173,7 +174,7 @@ class Queue
   end
 
   def push(val)
-    puts "\t#{@id} pushing #{val}"
+    # puts "\t#{@id} pushing #{val}"
     @lock.synchronize {
       @data <<  val
       @write_in += 1
@@ -183,7 +184,7 @@ class Queue
 end
 
 def hookup(a, b, c, d, e, input_file = 'input.txt')
-  puts "\n\n"
+  # puts "\n\n"
   a_in = Queue.new
   10.times {a_in.push(0)}
 
@@ -206,35 +207,92 @@ def hookup(a, b, c, d, e, input_file = 'input.txt')
   eh = e.run_async
 
   ah.join
-  puts "a.join"
+  # puts "a.join"
 
   bh.join
-  puts "b.join"
+  # puts "b.join"
 
   ch.join
-  puts "c.join"
+  # puts "c.join"
 
   dh.join
-  puts "d.join"
+  # puts "d.join"
 
   eh.join
-  puts "e.join"
+  # puts "e.join"
 
   final_output = e_out.next
 
-  puts "final output: #{final_output}"
+  # puts "final output: #{final_output}"
 
   final_output
 end
-
 
 
 def solveA(filename = 'input.txt')
   max_v = 0
   max_ar = []
   [0, 1, 2, 3, 4].permutation.each do |p|
-    puts p
     v = hookup(*p, filename)
+    if v > max_v
+      max_v = v
+      max_ar = p
+    end
+  end
+  max_v
+end
+
+
+def hookupB(a, b, c, d, e, input_file = 'input.txt')
+  # puts "\n\n"
+  a_in = Queue.new
+  1.times {a_in.push(0)}
+
+  a_out = Queue.new('a')
+  b_out = Queue.new('b')
+  c_out = Queue.new('c')
+  d_out = Queue.new('d')
+
+  a = Program.new(parse_input(input_file), a, a_in,  a_out, 'a')
+  b = Program.new(parse_input(input_file), b, a_out, b_out, 'b')
+  c = Program.new(parse_input(input_file), c, b_out, c_out, 'c')
+  d = Program.new(parse_input(input_file), d, c_out, d_out, 'd')
+  e = Program.new(parse_input(input_file), e, d_out, a_in, 'e')
+
+  ah = a.run_async
+  bh = b.run_async
+  ch = c.run_async
+  dh = d.run_async
+  eh = e.run_async
+
+  ah.join
+  # puts "a.join"
+
+  bh.join
+  # puts "b.join"
+
+  ch.join
+  # puts "c.join"
+
+  dh.join
+  # puts "d.join"
+
+  eh.join
+  # puts "e.join"
+
+  final_output = e.last_output
+
+  # puts "final output: #{final_output}"
+
+  final_output
+
+end
+
+def solveB(filename = 'input.txt')
+  max_v = 0
+  max_ar = []
+  [5, 6, 7, 8, 9].permutation.each do |p|
+    v = hookupB(*p, filename)
     if v > max_v
       max_v = v
       max_ar = p
@@ -246,10 +304,13 @@ end
 
 
 def main
-  # v = solve
-  # p v
   v = solveA
-  p "final solution: #{v}"
+  p "final solution A: #{v}"
+
+  v = solveB
+  p "final solution B: #{v}"
+
+
 end
 
 # require 'test/unit'
@@ -257,10 +318,12 @@ end
 #   def test_a
 #     assert_equal(43_210, solveA('sample.txt'))
 #     assert_equal(54_321, solveA('sample2.txt'))
-#     assert_equal(65_210, solveA('sample3.txt'))
+#     # assert_equal(65_210, solveA('sample3.txt'))
+#     assert_equal(199_988, solveA('input.txt'))
 #   end
 
 #   def test_b
+#     assert_equal(139_629_729, solveB('./sample3.txt'))
 #   end
 # end
 
